e3fd08e6c262e8a2251425d7241f36e25b26d472
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-09-23 13:05:38 +0100

    installed axios and added a get request to a categories endpoint, now getting some data back

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { hello: 'Hello World!' })
+const token = 'BQC0mNHLRIKCPSmb0IJon3xBgjs7lV9rRSgBdZ-scOY1hWuy-UhueyCwBxadfRiGUmTzWGnQ5gRPKs6XOb-KZG0it96hMojSdsv8gMtc2WSe52ebLIw';
+
+const apiURL = 'https://api.spotify.com/v1/browse/categories'
+
+
+
+
+app.get('/', async (req, res) => {
+
+    try {
+
+        const result = await axios(apiURL, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }, );
+    
+        console.log(result.data)
+    
+        res.render('index.ejs', { hello: 'Hello World!' });
+        
+    } catch (error) {
+        console.error(error.response ? error.response.data : error.message);
+        res.status(500).send('Error retrieving data from Spotify.');
+        
+    }
+    
+   
 })
 
 app.listen(port, () => {
