b5f44beeb1c5a52693ac6c82fbb3baea8fb88971
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-09-23 14:35:30 +0100

    changed the project to be a search function that will return data on your favourite artist

--- app.js
+++ app.js
@@ -6,51 +6,58 @@ const port = 3000;
 
 const token = 'BQAlb3t5u-K2ixua1Q-JSbCQ-74aNMAynFu0Qjlzd0-Vslt-Pcq8i5V950pxg-gth3sGZur1J85eCXPP95KCh_IH_GZsxJBmE5ip1DcJ991-ay2T2A0';
 
-const apiURL = 'https://api.spotify.com/v1/browse/categories?limit=50'
+const apiURL = 'https://api.spotify.com/v1/search';
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
 
-    try {
+    // try {
 
-        const result = await axios(apiURL, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        }, );
+    //     const result = await axios(apiURL, {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         }
+    //     }, );
     
-        // console.log(result.data.categories.items);
+    //     // console.log(result.data.categories.items);
         
-        console.log(result.data.categories.items[0].icons);
+    //     console.log(result.data.categories.items[0].icons);
 
-        const categories = result.data.categories.items;
-        var array = [];
+    //     const categories = result.data.categories.items;
+    //     var array = [];
 
-        categories.forEach(category => {
-            array.push({ name: category.name });
-            category.icons.forEach(icon => {
-                array.push({
-                    height: icon.height,
-                        url: icon.url,
-                    width: icon.width,
+    //     categories.forEach(category => {
+    //         array.push({ name: category.name });
+    //         category.icons.forEach(icon => {
+    //             array.push({
+    //                 height: icon.height,
+    //                     url: icon.url,
+    //                 width: icon.width,
 
-                })
-            })
+    //             })
+    //         })
             
-        });
+    //     });
 
-        console.log(array)
+    //     console.log(array)
 
     
-        res.render('index.ejs', { categories: array });
+        res.render('index.ejs',);
         
-    } catch (error) {
-        console.error(error.response ? error.response.data : error.message);
-        res.status(500).send('Error retrieving data from Spotify.');
+    // } catch (error) {
+    //     console.error(error.response ? error.response.data : error.message);
+    //     res.status(500).send('Error retrieving data from Spotify.');
         
-    }
+    // }
     
    
+})
+
+app.post('/search-artist', (req, res) => {
+    console.log(req)
+    console.log(req.body);
+    res.render('index.ejs')
 })
 
 app.listen(port, () => {
