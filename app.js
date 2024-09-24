7c0671b092a6d7be9128a09c65daa8eb6ccc3762
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-09-24 20:32:01 +0100

    renamed profile to dashboard, deleted old code for searching for artists as no longer needed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express, { response } from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
-import  querystring from 'querystring';
+import querystring from 'querystring';
 
 dotenv.config()
 
@@ -11,7 +11,6 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-// const token = process.env.BEARER_TOKEN;
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
@@ -35,10 +34,9 @@ const redirect_uri = 'http://localhost:3000/callback'
 const scopes = [
     'user-read-private',
     'user-read-email',
-    'user-top-read'
-    // Add other required scopes here
+    'user-top-read',
+    'user-library-read'
 ].join(' ');
-
 
 app.get('/authorize', async  (req, res) => {
 
@@ -72,12 +70,10 @@ app.get('/callback', async (req, res) => {
             'Authorization': 'Basic ' + (new Buffer.from(clientID + ':' + clientSecret).toString('base64'))
         }
     })
-    
-    // console.log(tokenResponse.data);
 
     token = tokenResponse.data.access_token;
 
-    res.redirect('/profile')    
+    res.redirect('/dashboard')    
 
     }
 
@@ -105,7 +101,7 @@ async function getData(endpoint) {
     return data
 }
 
-app.get('/profile', async (req, res) => {
+app.get('/dashboard', async (req, res) => {
     
     try {
 
@@ -114,97 +110,13 @@ app.get('/profile', async (req, res) => {
 
         const topTracksData = topTracks.items.map(track => ({ artist: track.artists[0].name, name: track.name, duration_ms: track.duration_ms, url: track.external_urls.spotify, image: track.album.images[0]?.url }))
         
-        res.render('profile.ejs', {user: userInfo, tracks: topTracksData})
+        res.render('dashboard.ejs', {user: userInfo, tracks: topTracksData})
         
     } catch (error) {
         console.error(error.response ? error.response.data : error.message);
          res.status(500).send(error.message);
     }
     
-})
-
-const searchURL = 'https://api.spotify.com/v1/search';
-
-app.post('/search-artist', async (req, res) => {
-
-    const artistName = req.body.artistName;
-    // console.log(artistName)
-
-    try {
-
-        const searchArtist = await axios.get(searchURL, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                q: artistName,
-                type: "artist",
-                limit: '1'
-            }
-        },)
-
-        const artistInfo = {
-            name: searchArtist.data.artists.items[0].name,
-            image: searchArtist.data.artists.items[0].images[0].url,
-            id: searchArtist.data.artists.items[0].id,
-            followers: searchArtist.data.artists.items[0].followers.total,
-            popularity: searchArtist.data.artists.items[0].popularity
-        }
-
-        const searchTopTracks = await axios.get(`https://api.spotify.com/v1/artists/${artistInfo.id}/top-tracks`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-        },)
-
-        //  console.log(searchTopTracks.data);
-
-        const topTracks = searchTopTracks.data.tracks;
-
-        //  console.log(topTracks)
-
-        const trackLimit = 5;
-
-        const topTrackInfo = topTracks.slice(0, trackLimit).map(track => ({ name: track.name, duration_ms: track.duration_ms, url: track.external_urls.spotify, image: track.album.images[0]?.url   }))
-
-        // console.log(topTrackInfo);
-
-        const searchAppearsOn = await axios.get(`https://api.spotify.com/v1/artists/${artistInfo.id}/albums`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                limit: 5,
-
-            }
-        },)
-
-        
-        const albums = searchAppearsOn.data.items;
-
-        console.log(albums[3].artists)
-
-        const appearsOn = albums.map(album => ({ name: album.name, artists: album.artists[0].name,url: album.external_urls.spotify, image: album.images[0]?.url }));
-        
-        const searchRelatedArtists = await axios.get(`https://api.spotify.com/v1/artists/${artistInfo.id}/related-artists`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-            
-        },)
-        
-        const artistLimit = 10;
-
-        const relatedArtistData = searchRelatedArtists.data.artists;
-
-        const relatedArtists = relatedArtistData.slice(0, artistLimit).map(artist => ({ name: artist.name, url: artist.external_urls.spotify, image: artist.images[0]?.url }));
-
-        res.render('result.ejs', { artistInfo: artistInfo, topTrackInfo: topTrackInfo, appearsOn: appearsOn, relatedArtists: relatedArtists })
-        
-    } catch (error) {
-        console.error(error.response ? error.response.data : error.message);
-        res.status(500).send(error.message);
-    }
 })
 
 app.listen(port, () => {
